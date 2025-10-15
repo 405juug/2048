8cc5495d6b8f5de5e2cd5090c9b5c050b8c8b905
405juug <scaryyow@mail.ru>
2025-10-15 14:44:56 +0300

    added up and down movement

--- src/classes/game.js
+++ src/classes/game.js
@@ -87,19 +87,59 @@ export default class Game {
         this.tiles[y][x] = new Tile(v, +x, +y)        
     }
 
-    moveDown(){}
-    moveUp(){}
+    moveDown(){
+        for(let x = 0; x < this.tileCount; x++){
+            for(let y = this.tileCount - 2; y >= 0; y--){
+                if(!this.tiles[y][x]) continue
+
+                for(let i = y + 1; i < this.tileCount; i++){
+                    if(this.tiles[i][x]){
+                        if(i == y + 1) break;
+                        this.tiles[i - 1][x] = this.tiles[y][x];
+                        this.tiles[y][x] = null;
+                        break;
+                    }
+
+                    if(!this.tiles[i][x] && i == this.tileCount - 1){
+                        this.tiles[i][x] = this.tiles[y][x];
+                        this.tiles[y][x] = null;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    moveUp(){
+        for(let x = 0; x < this.tileCount; x++){
+            for(let y = 1; y < this.tileCount; y++){
+                if(!this.tiles[y][x]) continue
+
+                for(let i = y - 1; i >= 0; i--){
+                    if(this.tiles[i][x]){
+                        if(i == y - 1) break;
+                        this.tiles[i + 1][x] = this.tiles[y][x];
+                        this.tiles[y][x] = null;
+                        break;
+                    }
+
+                    if(!this.tiles[i][x] && i == 0){
+                        this.tiles[i][x] = this.tiles[y][x];
+                        this.tiles[y][x] = null;
+                        break;
+                    }
+                }
+            }
+        }
+    }
     moveRight(){
-        // this.tiles = this.tiles.map((tile) => ({...tile, x: +tile.x +1 == 5 ? 4 : +tile.x + 1}))
-        // console.log(this.tiles);
             for(let y = 0; y < this.tiles.length; y++){
                 for(let x = this.tiles[y].length - 2; x >= 0; x--){
                     if(!this.tiles[y][x]) continue
                     for(let i = x + 1; i < this.tiles[y].length; i++){
-                    if(this.tiles[y][i]){
-                        if(i == x + 1) break
-                        this.tiles[y][i - 1] = this.tiles[y][x];
-                        this.tiles[y][x] = null;
+                        if(this.tiles[y][i]){
+                            if(i == x + 1) break
+                            this.tiles[y][i - 1] = this.tiles[y][x];
+                            this.tiles[y][x] = null;
                     }
                     if(!this.tiles[y][i] && i == this.tiles[y].length - 1){
                         this.tiles[y][i] = this.tiles[y][x];
@@ -110,7 +150,6 @@ export default class Game {
         }
     }
     moveLeft(){
-        // this.tiles = this.tiles.map((tile) => ({...tile, x: +tile.x -1 == 0 ? 1 : +tile.x - 1}))
         for(let y = 0; y < this.tiles.length; y++){
             for(let x = 1; x < this.tiles[y].length; x++){
                 if(!this.tiles[y][x]) continue
